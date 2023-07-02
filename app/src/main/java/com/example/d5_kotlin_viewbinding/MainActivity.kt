cbc2dd1478da83c45e9f67bf8f3a928fb9ecbc0e
jv1111 <webd0829@gmail.com>
2023-07-02 15:57:39 +0800

    binding

--- app/src/main/java/com/example/d5_kotlin_viewbinding/MainActivity.kt
+++ app/src/main/java/com/example/d5_kotlin_viewbinding/MainActivity.kt
@@ -5,20 +5,25 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.example.d5_kotlin_viewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val tvMessage = findViewById<TextView>(R.id.tvMessage)
-        val etName = findViewById<EditText>(R.id.etName)
-        val btnSubmit = findViewById<Button>(R.id.btnSubmit)
+//        val tvMessage = findViewById<TextView>(R.id.tvMessage)
+//        val etName = findViewById<EditText>(R.id.etName)
+//        val btnSubmit = findViewById<Button>(R.id.btnSubmit)
 
-        btnSubmit.setOnClickListener{
-            val name = etName.text.toString()
-            tvMessage.text = "Hello $name"
-            etName.setText("")
+        binding.btnSubmit.setOnClickListener{
+            val name = binding.etName.text.toString()
+            binding.tvMessage.text = "Hello $name"
+            binding.etName.setText("")
         }
     }
 }
