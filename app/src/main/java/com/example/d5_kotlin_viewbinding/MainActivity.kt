590f4410c9492d12e41a75dc7adbf8ac600a8fb7
jv1111 <webd0829@gmail.com>
2023-07-02 15:48:15 +0800

    first commit

---
+++ app/src/main/java/com/example/d5_kotlin_viewbinding/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.d5_kotlin_viewbinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tvMessage = findViewById<TextView>(R.id.tvMessage)
+        val etName = findViewById<EditText>(R.id.etName)
+        val btnSubmit = findViewById<Button>(R.id.btnSubmit)
+
+        btnSubmit.setOnClickListener{
+            val name = etName.text.toString()
+            tvMessage.text = "Hello $name"
+            etName.setText("")
+        }
+    }
+}
